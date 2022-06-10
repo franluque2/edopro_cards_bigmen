dceaf47334babdf0186258218a362a93dc505537
Fran Luque <franluque2@gmail.com>
2022-06-10 04:52:59 -0300

    Almost done with these skills

---
+++ script/c81632110.lua
@@ -0,0 +1,89 @@
+--Fool Clown
+local s,id=GetID()
+function s.initial_effect(c)
+
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,{1,id})
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.thtg2)
+	e1:SetOperation(s.thop2)
+	c:RegisterEffect(e1)
+	
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	
+	
+
+	--target 1 monster, gain lp equal to its attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCondition(s.desscon)
+	e2:SetTarget(s.desstg)
+	e2:SetOperation(s.dessop)
+	c:RegisterEffect(e2)
+
+	--change name to anime fool clown
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_CHANGE_CODE)
+	e4:SetRange(LOCATION_MZONE+LOCATION_HAND)
+	e4:SetValue(511009200)
+	c:RegisterEffect(e4) 
+end
+
+function s.desscon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0
+end
+
+function s.desstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil) 
+end
+
+function s.dessop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if e:GetHandler():IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Recover(tp,tc:GetAttack(),REASON_EFFECT)
+	end
+end
+
+function s.thfilter2(c)
+	return c:IsFaceup() and c:IsAbleToHand()
+end
+
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
+	local g=Duel.GetMatchingGroup(s.thfilter2,tp,LOCATION_REMOVED+LOCATION_GRAVE,0,nil)
+	local ct=e:GetLabel()
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
+function s.costfilter(c)
+	return c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	local rt=Duel.GetTargetCount(Card.IsAbleToHand,tp,LOCATION_REMOVED+LOCATION_GRAVE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local cg=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,rt,nil)
+	Duel.SendtoGrave(cg,REASON_COST)
+	e:SetLabel(#cg)
+end
---
+++ script/c81632911.lua
@@ -0,0 +1,174 @@
+--Promise for Power
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate Skill
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	aux.AddSkillProcedure(c,2,false,s.flipcon2,s.flipop2)
+end
+local LOCATION_HDG=LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE
+local LOCATION_RMV_GRV=LOCATION_REMOVED+LOCATION_GRAVE
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+
+		local c=e:GetHandler()
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetOperation(s.adop)
+	Duel.RegisterEffect(e2,tp)
+	end
+	e:SetLabel(1)
+end
+
+function not_silent_pain_filter(c)
+return not c:IsCode(511002742)
+end
+
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,not_silent_pain_filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	s.place_pain(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+end
+function s.place_pain(e,tp,eg,ep,ev,re,r,rp)
+	local pain=Duel.CreateToken(tp,511002742)
+	Duel.SendtoGrave(pain,REASON_EFFECT)
+end
+function s.silent_pain_filter(c)
+	return c:IsCode(511002742)
+end
+
+function s.torment_or_space_filter(c)
+	return (c:IsCode(511002744) or c:IsCode(511002743)) and c:IsAbleToHand()
+end
+function s.anchor_knight_filter(c)
+	return c:IsCode(511001385) and c:IsFaceup()
+end
+function s.violent_salvage_filter(c,e,tp)
+	return c:IsCode(511001386) and c:IsSSetable()
+end
+function s.fool_clown_filter(c)
+	return c:IsCode(511009200) and c:IsFaceup()
+end
+
+
+function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
+	--OPT check
+	if Duel.GetFlagEffect(ep,id+1)>0 and Duel.GetFlagEffect(ep,id+2)>0 and Duel.GetFlagEffect(ep,id+3)>0 then return end
+	--Boolean checks for the activation condition: b1, b2, b3
+	local b1=Duel.GetFlagEffect(ep,id+1)==0
+			and Duel.IsExistingMatchingCard(s.silent_pain_filter,tp,LOCATION_GRAVE,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.torment_or_space_filter,tp,LOCATION_DECK,0,1,nil)
+
+	local b2=Duel.GetFlagEffect(ep,id+2)==0
+			and Duel.IsExistingMatchingCard(s.anchor_knight_filter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.violent_salvage_filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+
+	local b3=Duel.GetFlagEffect(ep,id+3)==0
+			and Duel.IsExistingMatchingCard(s.fool_clown_filter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+			and Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,1,nil)
+
+	return aux.CanActivateSkill(tp) and (b1 or b2 or b3)
+end
+function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,HINT_SELECTMSG)
+	--Boolean check for effect 1:
+	local b1=Duel.GetFlagEffect(ep,id+1)==0
+			and Duel.IsExistingMatchingCard(s.silent_pain_filter,tp,LOCATION_GRAVE,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.torment_or_space_filter,tp,LOCATION_DECK,0,1,nil)
+	
+	--Boolean check for effect2:
+	local b2=Duel.GetFlagEffect(ep,id+2)==0
+			and Duel.IsExistingMatchingCard(s.anchor_knight_filter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.violent_salvage_filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+	
+	--Boolean check for effect3:
+	local b3=Duel.GetFlagEffect(ep,id+3)==0
+			and Duel.IsExistingMatchingCard(s.fool_clown_filter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+			and Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,1,nil)
+
+	--This auxiliary function should simplify what you did with all the Duel.SelectOption you used previously:
+	local op=aux.SelectEffect(tp, {b1,aux.Stringid(id,0)},
+								  {b2,aux.Stringid(id,1)},
+								  {b3,aux.Stringid(id,2)})
+	op=op-1 --SelectEffect returns indexes starting at 1, so we decrease the result by 1 to match your "if"s
+
+	if op==0 then
+		s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==1 then
+		s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==2 then
+		s.operation_for_res2(e,tp,eg,ep,ev,re,r,rp)
+	end
+end
+
+--op=0, Once per turn, if "Silent Pain" is your GY: You can add 1 "Silent Torment" or 1 "Silent Space" from your Deck to your Hand
+function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.torment_or_space_filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+	Duel.RegisterFlagEffect(tp,id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+	
+end
+
+--op=1, Once per turn, if you control "Anchor Knight" you can set 1 "Violent Salvage" from your Deck or GY to your Spell/Trap Zone.
+function s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.violent_salvage_filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g:GetFirst())
+	end
+	Duel.RegisterFlagEffect(tp,id+2,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+
+--op=2, Once per turn, if you control "Fool Clown", you can banish any number of cards from your GY, then Special Summon 1 "Rough Exploder" for every 3 cards banished, if you banished 6 or more cards, you can also place 1 "Rough Fight" in your GY from outside the duel.
+function s.operation_for_res2(e,tp,eg,ep,ev,re,r,rp)
+	local banish_max=Duel.GetLocationCount(tp,LOCATION_MZONE)*3
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_GRAVE,0,3,banish_max,nil)
+	local cg=Duel.Remove(g,POS_FACEUP,REASON_COST)
+	if cg>0 then
+		for i = 1,math.floor(cg/3),1 
+		do 
+			local exploder=Duel.CreateToken(tp,511002098)
+			Duel.SpecialSummon(exploder,0,tp,tp,false,false,POS_FACEUP)
+		end
+		if cg>6 then
+			local roughfight=Duel.CreateToken(tp,511002097)
+			Duel.SendtoGrave(roughfight,REASON_EFFECT)
+		end
+	end
+	Duel.RegisterFlagEffect(tp,id+3,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
