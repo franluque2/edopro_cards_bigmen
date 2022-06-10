7970290edd15f20829f981c2faa2aab77f7a79f9
Fran Luque <franluque2@gmail.com>
2022-06-10 11:50:17 -0300

    fix skills

--- script/c81632909.lua
+++ script/c81632909.lua
@@ -1,19 +1,7 @@
 --Curse of Lucky Straight
 local s,id=GetID()
 
-local register=function(what)
-	return function(...)
-		local params={...}
-		local tp=params[1]
-			if Duel.GetFlagEffect(tp,id+1)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) and Duel.IsExistingMatchingCard(s.number_seven_filter,tp,LOCATION_MZONE,0,1,nil) then
-			Duel.SetFlagEffectLabel(tp,id,1)
-		end
-		return what(...)
-	end
-end
-s.roll_dice=true
-local tossd=Duel.TossDice
-Duel.TossDice=register(tossd)
+
 function s.initial_effect(c)
 	--Activate Skill
 	aux.AddSkillProcedure(c,1,false,nil,nil)
@@ -37,46 +25,10 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCondition(s.flipcon)
 		e1:SetOperation(s.flipop)
 		Duel.RegisterEffect(e1,tp)
-		Duel.RegisterFlagEffect(tp,id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
-	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(e:GetHandler())
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EFFECT_TOSS_DICE_CHOOSE)
-		ge1:SetCondition(s.dicecon)
-		ge1:SetOperation(s.diceop(Duel.GetDiceResult,Duel.SetDiceResult))
-		Duel.RegisterEffect(ge1,0)
-	end)
+		Duel.RegisterFlagEffect(tp,id+1,0,0,0)
 end
 e:SetLabel(1)
 	end
-
-
-function s.dicecon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(ep,id)>0 and Duel.IsExistingMatchingCard(s.number_seven_filter,tp,LOCATION_MZONE,0,1,nil) and Duel.GetFlagEffectLabel(ep,id)>0
-end
-
-
-function s.diceop(func1,func2)
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		Duel.ResetFlagEffect(ep,id+1)
-		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-		Duel.Hint(HINT_CARD,tp,id)
-		local dc={func1()}
-		local ct=(ev&0xff)+(ev>>16)
-		local ac=1
-		if ct>1 then 
-			Duel.Hint(HINT_SELECTMSG,ep,aux.Stringid(id,1))
-			local val,idx=Duel.AnnounceNumber(ep,table.unpack(dc,1,ct))
-			ac=idx+1
-			dc[ac]=6
-			
-		else
-			dc[1]=6
-		end
-		func2(table.unpack(dc))
-	end
-
-end
 
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
@@ -86,6 +38,7 @@ end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
+	s.replace_number_seven(e,tp,eg,ep,ev,re,r,rp)
 	s.summon_slotmachine(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
 end
@@ -99,6 +52,22 @@ function s.summon_slotmachine(e,tp,eg,ep,ev,re,r,rp)
 	local mslots=Duel.CreateToken(tp,09576193)
 	Duel.SSet(tp,mslots)
 end
+
+function s.number_seven_filter_extra(c)
+return c:IsCode(82308875)
+end
+
+function s.replace_number_seven(e,tp,eg,ep,ev,re,r,rp)
+local hg=Duel.GetMatchingGroup(s.number_seven_filter_extra,tp,LOCATION_EXTRA,0,nil)
+if #hg>0 then			
+			for card in aux.Next(hg)do
+			Duel.SendtoDeck(card,tp,-2,REASON_EFFECT)
+			local newnoseven=Duel.CreateToken(tp,81632111)
+			Duel.SendtoDeck(newnoseven,tp,SEQ_DECKTOP,REASON_EFFECT)
+	end
+
+end
+end
 function s.spell_banish_filter(c)
 	return c:IsType(TYPE_SPELL) and c:IsAbleToRemove()
 end
@@ -106,9 +75,7 @@ function s.monster_banish_filter(c,e,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsLevelAbove(6) and c:IsLevelBelow(8)  and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
-function s.number_seven_filter(c)
-	return c:IsCode(82308875) and c:IsFaceup()
-end
+
 --. Once per turn, you can banish 1 Spell from your GY to target 1 Level 7 monster that's either banished or in your GY, then Special Summon it in Attack Position.
 function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
 	
