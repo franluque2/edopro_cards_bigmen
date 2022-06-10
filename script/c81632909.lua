b116e917fe342762f79c2708d81a30eab768e09e
Fran Luque <franluque2@gmail.com>
2022-06-10 01:43:10 -0300

    still working on lucky straight

--- script/c81632909.lua
+++ script/c81632909.lua
@@ -5,31 +5,29 @@ local register=function(what)
 	return function(...)
 		local params={...}
 		local tp=params[1]
-		if not Duel.IsExistingMatchingCard(s.number_seven_filter,tp,LOCATION_MZONE,0,1,nil) then return what(...) end
-		if Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			if Duel.GetFlagEffect(tp,id+1)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) and Duel.IsExistingMatchingCard(s.number_seven_filter,tp,LOCATION_MZONE,0,1,nil) then
 			Duel.SetFlagEffectLabel(tp,id,1)
 		end
 		return what(...)
 	end
 end
-
+s.roll_dice=true
+local tossd=Duel.TossDice
+Duel.TossDice=register(tossd)
 function s.initial_effect(c)
 	--Activate Skill
-	aux.AddSkillProcedure(c,2,false,nil,nil)
+	aux.AddSkillProcedure(c,1,false,nil,nil)
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetCountLimit(1)
 	e1:SetRange(0x5f)
-	e1:SetLabel(0)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
-	aux.AddSkillProcedure(c,2,false,s.flipcon2,s.flipop2)
+	aux.AddSkillProcedure(c,1,false,s.flipcon2,s.flipop2)
 end
-s.roll_dice=true
-local tossd=Duel.TossDice
-Duel.TossDice=register(tossd)
+
 
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then
@@ -39,6 +37,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCondition(s.flipcon)
 		e1:SetOperation(s.flipop)
 		Duel.RegisterEffect(e1,tp)
+		Duel.RegisterFlagEffect(tp,id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(e:GetHandler())
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -53,13 +52,15 @@ e:SetLabel(1)
 
 
 function s.dicecon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)>0 and Duel.IsExistingMatchingCard(s.number_seven_filter,tp,LOCATION_MZONE,0,1,nil) and Duel.GetFlagEffectLabel(tp,id)>0
+	return Duel.GetFlagEffect(ep,id)>0 and Duel.IsExistingMatchingCard(s.number_seven_filter,tp,LOCATION_MZONE,0,1,nil) and Duel.GetFlagEffectLabel(ep,id)>0
 end
 
 
 function s.diceop(func1,func2)
 	return function(e,tp,eg,ep,ev,re,r,rp)
-		Duel.ResetFlagEffect(ep,id)
+		Duel.ResetFlagEffect(ep,id+1)
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+		Duel.Hint(HINT_CARD,tp,id)
 		local dc={func1()}
 		local ct=(ev&0xff)+(ev>>16)
 		local ac=1
@@ -68,11 +69,11 @@ function s.diceop(func1,func2)
 			local val,idx=Duel.AnnounceNumber(ep,table.unpack(dc,1,ct))
 			ac=idx+1
 			dc[ac]=6
+			
 		else
 			dc[1]=6
 		end
 		func2(table.unpack(dc))
-	Duel.RegisterFlagEffect(tp,id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 	end
 
 end
@@ -81,6 +82,7 @@ end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()==0 and (Duel.GetTurnCount()==1 or Duel.GetTurnCount()==2) and Duel.GetTurnPlayer()==tp
 end
+--At the start of your first Standby Phase, set 1 "Monster Slots" from outside the duel to your Spell/Trap Zone, then Special Summon 1 "Slot Machine" in Attack Position from outside the duel, then place 1 "Slot Machine" in your GY and on the top of your deck from outside the duel.
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
@@ -101,18 +103,17 @@ function s.spell_banish_filter(c)
 	return c:IsType(TYPE_SPELL) and c:IsAbleToRemove()
 end
 function s.monster_banish_filter(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsType(TYPE_MONSTER) and c:IsLevelAbove(6) and c:IsLevelBelow(8)  and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
 function s.number_seven_filter(c)
 	return c:IsCode(82308875) and c:IsFaceup()
 end
-
+--. Once per turn, you can banish 1 Spell from your GY to target 1 Level 7 monster that's either banished or in your GY, then Special Summon it in Attack Position.
 function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
 	
 	--OPT check
 	if Duel.GetFlagEffect(tp,id+2)>0 then return end
-	--Boolean checks for the activation condition: b1
 	local b1=Duel.GetFlagEffect(tp,id+2)==0
 			and Duel.IsExistingMatchingCard(s.spell_banish_filter,tp,LOCATION_GRAVE,0,1,nil)
 			and Duel.IsExistingMatchingCard(s.monster_banish_filter,tp,LOCATION_REMOVED+LOCATION_GRAVE,0,1,nil,e,tp)
