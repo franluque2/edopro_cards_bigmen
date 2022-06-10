e1c65b0cb28f0dd6c95a279c7fdd9d9f4355f686
Fran Luque <franluque2@gmail.com>
2022-06-10 16:13:15 -0300

    fix hydradrive advent

--- script/c81632903.lua
+++ script/c81632903.lua
@@ -93,7 +93,7 @@ function s.adop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.hydra_d_filter(c,label)
-	return (c:IsSetCard(0x577) and c:IsAbleToHand() and not c:IsCode(label)) or c:IsCode(511009503)
+	return (c:IsSetCard(0x577) and c:IsAbleToHand() and (not c:IsCode(label))) or c:IsCode(511009503)
 end
 
 
