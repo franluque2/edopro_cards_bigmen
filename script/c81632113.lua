920ed037e2e0828e4c121aecb850f19a5fc79822
Fran Luque <franluque2@gmail.com>
2022-06-10 21:19:37 -0300

    add capsulated mutation

---
+++ script/c81632113.lua
@@ -0,0 +1,117 @@
+--Para Mutant
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search 1 Mutant Mindmaster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+
+
+		--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCost(s.cost)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+
+		--to deck
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1,{id,3})
+	e4:SetTarget(s.thtg2)
+	e4:SetOperation(s.thop2)
+	c:RegisterEffect(e4)
+	
+end
+
+function s.thfilter(c)
+	return c:IsCode(11508758) and c:IsAbleToHand()
+end
+
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.filter(c,e,tp)
+	return  c:IsCode(11508758) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		--Can attack twice
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		g:GetFirst():RegisterEffect(e1)
+	end
+end
+end
+
+function s.thfilter2(c,hc)
+	if not c:IsCode(11508758) then return false end
+	return c:IsAbleToDeck() and hc:IsAbleToDeck()
+end
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.thfilter2(chkc,c) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter2,tp,LOCATION_GRAVE,0,1,nil,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil,c)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not (c:IsRelateToEffect(e) and tc:IsRelateToEffect(e)) then return end
+	local g=Group.FromCards(c,tc)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and Duel.IsPlayerCanDraw(tp,1) then
+			Duel.ShuffleDeck(tp)
+			Duel.Draw(tp,1,REASON_EFFECT)
+
+		end
+end
+
+
---
+++ script/c81632912.lua
@@ -0,0 +1,119 @@
+--Capsulated Mutation
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+	aux.AddSkillProcedure(c,1,false,s.flipcon2,s.flipop2)
+end
+
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	   --condition
+	return aux.CanActivateSkill(tp) and Duel.GetFlagEffect(tp,id)==0
+end
+
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local c=e:GetHandler()
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	Duel.RegisterEffect(e3,tp)
+	--During the End Phase, switch all "Mutant Mindmaster" you control to Defense Position. 
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCondition(s.swapcon)
+	e1:SetOperation(s.swapop)
+	Duel.RegisterEffect(e1,tp)
+
+
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+end
+
+function s.flipconfilter(c)
+	return c:IsCode(11508758) and c:IsFaceup()
+end
+
+
+function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
+	--opt check
+	if Duel.GetFlagEffect(tp,id)==0 then return end
+	if Duel.GetFlagEffect(tp,id+1)>0 then return end
+	--condition
+	local b1=Duel.GetFlagEffect(tp,id+1)==0 and Duel.IsExistingMatchingCard(s.flipconfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+	
+	return aux.CanActivateSkill(tp) and b1
+end
+
+
+function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler(e)
+	
+	local tc=Duel.SelectMatchingCard(tp,s.flipconfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+	   --Cannot be destroyed by card effects
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3001)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+		tc:RegisterEffect(e1)
+	end
+		--opd register
+		Duel.RegisterFlagEffect(tp,id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(id,tp)~=0
+end
+--At the end of the Battle Phase, you can tribute any amount of monsters you control that are owned by your opponent, draw that many cards. 
+
+
+function s.trifilter(c)
+	return c:IsReleasableByEffect() and c:GetOwner()~=c:GetControler()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local ft=Duel.GetLocationCount(tp,LOCATION_DECK)
+	local sg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return #sg>0 and Duel.IsPlayerCanDraw(tp,1) end
+	local rg=Duel.SelectReleaseGroupCost(tp,s.trifilter,0,5,false,nil,nil,ft,tp)
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local sg=Duel.SelectReleaseGroupCost(tp,s.trifilter,0,5,false,nil,nil,ft,tp)
+	if Duel.Release(sg,REASON_EFFECT)>0 then
+		sg=Duel.GetOperatedGroup()
+		Duel.Draw(p,#sg,REASON_EFFECT)
+	end
+end
+
+function s.swapcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(tp,id)~=0 and Duel.IsExistingMatchingCard(s.flipconfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.swapop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.flipconfilter,tp,LOCATION_MZONE,0,nil)
+	Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+	Duel.RegisterFlagEffect(tp,id+2,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+
+
+function s.econ(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)>1
+end
+
+function s.etarget(e,c)
+	return c:IsCode(11508758) and c:IsFaceup()
+end
