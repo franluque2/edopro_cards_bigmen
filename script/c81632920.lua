ff8f0b4fb2504857e3f141e2b6d8a083a65c390f
Fran L <franluque2@gmail.com>
2022-07-05 16:32:15 -0300

    add bandit of the shadows and mark of the condor

---
+++ script/c81632920.lua
@@ -0,0 +1,295 @@
+--Mark of the Condor
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate Skill
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	aux.AddSkillProcedure(c,2,false,s.flipcon2,s.flipop2)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_LEAVE_FIELD)
+		e2:SetCondition(s.flipcon3)
+		e2:SetOperation(s.flipop3)
+		Duel.RegisterEffect(e2,tp)
+
+	end
+	e:SetLabel(1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	s.activate_field(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+end
+function s.field_filter(c)
+	return c:IsType(TYPE_FIELD)
+end
+--at the start of the duel, activate 1 field spell from your hand or deck
+function s.activate_field(e,tp,eg,ep,ev,re,r,rp)
+	local field=Duel.SelectMatchingCard(tp,s.field_filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil)
+	if #field>0 then
+		aux.PlayFieldSpell(field:GetFirst(),e,tp,eg,ep,ev,re,r,rp)
+	end
+end
+
+function s.walkerfilter(c,e,tp)
+	return (c:IsCode(71821687) or c:IsCode(17315396)) and c:IsAbleToHand()
+end
+
+function s.monsterfilter(c)
+	return c:IsFaceup() and c:HasLevel()
+end
+
+function s.wiracocha_rasca_filter(c)
+	return c:IsCode(41181774) and c:IsFaceup()
+end
+
+function s.limbo_monster_filter(c,e,tp)
+	return (c:GetFlagEffect(id)>0) and c:IsCanBeSpecialSummoned(e,0,tp,true,false,POS_FACEUP)
+end
+
+function s.high_level_filter(c)
+	return c:HasLevel() and c:IsLevelAbove(5) and c:IsFaceup()
+end
+
+function s.supay_ascator_filter(c)
+	return (c:IsCode(78275321) or c:IsCode(78552773)) and c:IsAbleToDeck()
+end
+
+
+function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
+	--OPT check
+	if Duel.GetFlagEffect(tp,id+1)>0 and Duel.GetFlagEffect(tp,id+2)>0 and Duel.GetFlagEffect(tp,id+3)>0 and
+	 	Duel.GetFlagEffect(tp, id+4)>0 then return end
+	--Boolean checks for the activation condition: b1, b2, b3, b4
+	local b1=Duel.GetFlagEffect(ep,id+1)==0
+			and Duel.IsExistingMatchingCard(s.walkerfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil,e,tp)
+
+	local b2=Duel.GetFlagEffect(ep,id+2)==0
+			and Duel.IsExistingMatchingCard(s.monsterfilter,tp,LOCATION_MZONE,0,1,nil)
+
+	local b3=Duel.GetFlagEffect(ep,id+3)==0
+			and Duel.IsExistingMatchingCard(s.wiracocha_rasca_filter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil)
+			and Duel.GetActivityCount(tp,ACTIVITY_BATTLE_PHASE)==0
+
+	local b4=Duel.GetFlagEffect(ep, id+4)==0
+		and Duel.IsExistingMatchingCard(s.high_level_filter, tp, LOCATION_MZONE, 0, 1, nil)
+		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+
+
+	return aux.CanActivateSkill(tp) and (b1 or b2 or b3 or b4)
+end
+function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+
+	local b1=Duel.GetFlagEffect(ep,id+1)==0
+			and Duel.IsExistingMatchingCard(s.walkerfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil,e,tp)
+
+	local b2=Duel.GetFlagEffect(ep,id+2)==0
+			and Duel.IsExistingMatchingCard(s.monsterfilter,tp,LOCATION_MZONE,0,1,nil)
+
+	local b3=Duel.GetFlagEffect(ep,id+3)==0
+			and Duel.IsExistingMatchingCard(s.wiracocha_rasca_filter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil)
+			and Duel.GetActivityCount(tp,ACTIVITY_BATTLE_PHASE)==0
+
+	local b4=Duel.GetFlagEffect(ep, id+4)==0
+		and Duel.IsExistingMatchingCard(s.high_level_filter, tp, LOCATION_MZONE, 0, 1, nil)
+		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+
+
+	local op=aux.SelectEffect(tp, {b1,aux.Stringid(id,0)},
+								  {b2,aux.Stringid(id,1)},
+								  {b3,aux.Stringid(id,2)},
+								  {b4,aux.Stringid(id,4)})
+	op=op-1
+
+	if op==0 then
+		s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==1 then
+		s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==2 then
+		s.operation_for_res2(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==3 then
+		s.operation_for_res3(e,tp,eg,ep,ev,re,r,rp)
+	end
+end
+
+--op=0, Once per turn, you can add 1 "Ascator, Dawnwalker" or "Supay, Duskwalker" from your Deck or GY to your hand,
+ -- then you can shuffle all "Supay" and "Fire Ant Ascator" from your GY into the deck, and if you do,
+	--  add 1 "Eathbound Linewalker" to your hand from outside the duel.
+function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,s.walkerfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g, tp, REASON_EFFECT)
+		local g2=Duel.GetMatchingGroup(s.supay_ascator_filter, tp, LOCATION_GRAVE, 0, nil)
+		if #g2>0 then
+			if Duel.SelectYesNo(tp,aux.Stringid(id,5)) then
+				if Duel.SendtoDeck(g2, tp, SEQ_DECKSHUFFLE, REASON_EFFECT) then
+					local walker=Duel.CreateToken(tp,67987302)
+					Duel.SendtoHand(walker, tp, REASON_EFFECT)
+				end
+			end
+		end
+	end
+	Duel.RegisterFlagEffect(tp,id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+
+--op=1, Once per turn, You can target 1 monster you control, and declare a level from 1-11,
+-- until the end of this turn, that monster becomes that level, then you can make it become a dark tuner monster.
+function s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.SelectMatchingCard(tp,s.monsterfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LVRANK)
+		local lvl=Duel.AnnounceLevel(tp,1,11)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(lvl)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			local e3=e1:Clone()
+			e3:SetCode(EFFECT_ADD_TYPE)
+			e3:SetValue(TYPE_TUNER)
+			tc:RegisterEffect(e3)
+			local e4=e1:Clone()
+			e4:SetCode(EFFECT_ADD_SETCODE)
+			e4:SetValue(0x600)
+			tc:RegisterEffect(e4)
+			local e5=Effect.CreateEffect(e:GetHandler())
+			e5:SetType(EFFECT_TYPE_SINGLE)
+			e5:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+			e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e5:SetValue(s.synlimit)
+			e5:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e5)
+		end
+	end
+	Duel.RegisterFlagEffect(tp,id+2,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+
+function s.synlimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(0x601)
+end
+
+function ripairs(t)
+    -- Try not to use break when using this function;
+    -- it may cause the array to be left with empty slots
+    local ci = 0
+    local remove = function()
+        t[ci] = nil
+    end
+    return function(t, i)
+        --print("I", table.concat(array, ','))
+        i = i+1
+        ci = i
+        local v = t[i]
+        if v == nil then
+            local rj = 0
+            for ri = 1, i-1 do
+                if t[ri] ~= nil then
+                    rj = rj+1
+                    t[rj] = t[ri]
+                    --print("R", table.concat(array, ','))
+                end
+            end
+            for ri = rj+1, i do
+                t[ri] = nil
+            end
+            return
+        end
+        return i, v, remove
+    end, t, ci
+end
+
+local cards_in_limbo={}
+--op=2,Once per turn, if you control "Earthbound Immortal Wiraqocha Rasca",
+-- you can target 1 monster your opponent controls; send it to the Underworld (It Dissapears from the Duel).
+-- You cannot conduct your Battle Phase the turn you activate this effect.
+function s.operation_for_res2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.SelectMatchingCard(tp, aux.TRUE, tp, 0, LOCATION_MZONE, 1, 1, nil):GetFirst()
+	table.insert(cards_in_limbo,tc)
+	Duel.SendtoDeck(tc,1-tp,-2,REASON_EFFECT)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_BP)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,6),nil)
+	Duel.RegisterFlagEffect(tp,id+3,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+
+--op=3 set 1 "Contaminated Earth" from outside the duel to your Spell/Trap Zone.
+function s.operation_for_res3(e,tp,eg,ep,ev,re,r,rp)
+	local cearth=Duel.CreateToken(tp, 100000307)
+	Duel.SSet(tp,cearth)
+	Duel.RegisterFlagEffect(tp,id+4,0,0,0)
+end
+
+function s.cfilter(c,tp)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsCode(41181774) and c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp)
+end
+function s.flipcon3(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+--
+-- If a face-up "Earthbound Immortal Wiraqocha Rasca" you control leaves the field:
+-- your opponent Special Summons as many of their monsters from the Underworld as possible,
+-- ignoring their summoning conditions.
+
+function s.flipop3(e,tp,eg,ep,ev,re,r,rp)
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+		Duel.Hint(HINT_CARD,tp,id)
+		local count=0
+		local ft2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)
+		if ft2>0 then
+		if Duel.IsPlayerAffectedByEffect(1-tp,CARD_BLUEEYES_SPIRIT) then ft2=1 end
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
+		local ng=Group.CreateGroup()
+
+		for i, v, remove in ripairs(cards_in_limbo) do
+			Group.AddCard(ng,v)
+			remove()
+		end
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
+		local g=Group.Select(ng,1-tp,ft2,ft2,false,nil)
+		if #g>0 then
+
+			local tc=g:GetFirst()
+			while tc do
+				Duel.SpecialSummonStep(tc,0,1-tp,1-tp,false,false,POS_FACEUP)
+				tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+				tc=g:GetNext()
+				count=count+1
+			end
+		end
+	end
+	if count>0 then Duel.SpecialSummonComplete() end
+end
---
+++ script/c81632921.lua
@@ -0,0 +1,190 @@
+--Bandits of the Shadows
+
+local s,id=GetID()
+
+
+function s.initial_effect(c)
+	--Activate Skill
+	aux.AddSkillProcedure(c,1,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	aux.AddSkillProcedure(c,1,false,s.flipcon2,s.flipop2)
+end
+
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		e1:SetCountLimit(1)
+		Duel.RegisterEffect(e1,tp)
+		Duel.RegisterFlagEffect(tp,id+1,0,0,0)
+
+		local c=e:GetHandler()
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCountLimit(1)
+		e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e2:SetCondition(s.adcon)
+		e2:SetOperation(s.adop)
+		Duel.RegisterEffect(e2,tp)
+
+end
+e:SetLabel(1)
+	end
+
+
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+--
+-- At the start of the duel, Special Summon 1 "Don Zaloog" from outside the duel
+-- and set 1 "Mustering of the Dark Scorpions" to your Spell/Trap Zone from outside the duel.
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	s.summon_zaloog(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+end
+function s.summon_zaloog(e,tp,eg,ep,ev,re,r,rp)
+	local zaloog=Duel.CreateToken(tp, 76922029)
+	local mustering=Duel.CreateToken(tp, 68191243)
+	Duel.SpecialSummon(zaloog,0,tp,tp,false,false,POS_FACEUP)
+	Duel.SSet(tp, mustering, tp, true)
+end
+
+
+function s.dscorpion_addfilter(c)
+	return (c:IsSetCard(0x1a) or c:IsCode(76922029)) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+
+function s.dscorpion_filter(c)
+	return c:IsSetCard(0x1a) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
+end
+
+function s.zaloogfilter(c)
+	return c:IsCode(76922029) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
+end
+
+function s.dscorpion_or_zaloog_filter(c)
+	return (c:IsSetCard(0x1a) or c:IsCode(76922029)) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
+end
+function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
+
+	--OPT check
+	if Duel.GetFlagEffect(tp,id+2)>0 and Duel.GetFlagEffect(tp, id+6)>0 then return end
+	local g=Duel.GetMatchingGroup(s.dscorpion_filter,tp,LOCATION_MZONE,0,nil)
+	local cg= g:GetClassCount(Card.GetCode)==4
+--
+-- Once per turn, you can target a "Dark Scorpion" or "Don Zaloog" you control,
+-- this turn that target can attack your opponent directly this turn.
+
+	local b1=Duel.GetFlagEffect(tp,id+2)==0
+			and Duel.IsExistingMatchingCard(s.dscorpion_or_zaloog_filter,tp,LOCATION_MZONE,0,1,nil)
+
+-- Once per turn, if you control Don Zaloog and 4 "Dark Scorpion" monsters with different names,
+-- you can set 1 "Dark Scorpion Combination" from outside the duel to your Spell/Trap Zone. It can be activated this turn.
+	local b2=Duel.GetFlagEffect(tp, id+6)==0
+		and cg
+		and Duel.IsExistingMatchingCard(s.zaloogfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+
+
+	return aux.CanActivateSkill(tp) and (b1 or b2)
+end
+function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,tp,id)
+
+	local g=Duel.GetMatchingGroup(s.dscorpion_filter,tp,LOCATION_MZONE,0,nil)
+	local cg= g:GetClassCount(Card.GetCode)==4
+--
+-- Once per turn, you can target a "Dark Scorpion" or "Don Zaloog" you control,
+-- this turn that target can attack your opponent directly this turn.
+
+	local b1=Duel.GetFlagEffect(tp,id+2)==0
+			and Duel.IsExistingMatchingCard(s.dscorpion_or_zaloog_filter,tp,LOCATION_MZONE,0,1,nil)
+
+-- Once per turn, if you control Don Zaloog and 4 "Dark Scorpion" monsters with different names,
+-- you can set 1 "Dark Scorpion Combination" from outside the duel to your Spell/Trap Zone. It can be activated this turn.
+	local b2=Duel.GetFlagEffect(tp, id+6)==0
+		and cg
+		and Duel.IsExistingMatchingCard(s.zaloogfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+
+
+		local op=aux.SelectEffect(tp, {b1,aux.Stringid(id,0)},
+									  {b2,aux.Stringid(id,1)})
+		op=op-1
+
+	if op==0 then
+			local scorpion=Duel.SelectMatchingCard(tp,s.dscorpion_or_zaloog_filter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(3205)
+			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DIRECT_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			scorpion:RegisterEffect(e1)
+			--opt register
+			Duel.RegisterFlagEffect(tp,id+2,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+	elseif op==1 then
+			local combination=Duel.CreateToken(tp, 20858318)
+			Duel.SSet(tp, combination, tp, true)
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+			e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			combination:RegisterEffect(e2)
+			Duel.RegisterFlagEffect(tp, id+6, RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END, 0, 0)
+	end
+end
+
+
+--
+-- Once per turn,
+-- during your Standby Phase, add 1 random "Dark Scorpion" monster or "Don Zaloog" from your Deck to your Hand.
+
+function s.adcon(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.GetTurnPlayer()==tp and not (Duel.GetFlagEffect(tp,id+3)>0) then return end
+
+	local b1=Duel.GetFlagEffect(tp,id+3)==0
+			and Duel.IsExistingMatchingCard(s.dscorpion_addfilter,tp,LOCATION_DECK,0,1,nil)
+
+
+	return Duel.GetTurnPlayer()==tp and (b1)
+end
+
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+		Duel.Hint(HINT_CARD,tp,id)
+		s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+
+end
+
+
+
+function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+		local g=Duel.GetMatchingGroup(s.dscorpion_addfilter,tp,LOCATION_DECK,0,nil)
+		if #g>0 then
+			local cardnumber=math.random( #g )
+			local tc=g:GetFirst()
+			while tc do
+				if cardnumber==0 then
+					Duel.SendtoHand(tc, tp, REASON_EFFECT)
+				end
+				cardnumber=cardnumber-1
+				tc=g:GetNext()
+			end
+		end
+		Duel.RegisterFlagEffect(tp, id+3, RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END, 0, 0)
+end
