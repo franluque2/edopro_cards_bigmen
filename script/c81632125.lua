b6775261722dff0cd20568d0100b0654c70abe84
Fran Luque <franluque2@gmail.com>
2022-07-10 15:27:27 -0300

    fix

--- script/c81632125.lua
+++ script/c81632125.lua
@@ -21,12 +21,16 @@ end
 function s.tgfilter2(c)
 	return aux.IsCodeListed(c, 450000110) and (c:IsAbleToHand() or c:IsSSetable()) and (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)) and not c:IsCode(81000061)
 end
+
+function s.staddreamsfilter(c)
+	return c:IsCode(450000110) and c:IsFaceup()
+end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:GetControler()==tp and s.tgfilter(chkc) end
 	if chk==0 then return (Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,nil) or (Duel.IsExistingTarget(s.tgfilter2,tp,LOCATION_GRAVE,0,1,nil) and Duel.IsExistingMatchingCard(Card.IsCode, tp, LOCATION_ONFIELD, 0, 1, nil, 450000110))) end
 
 	local b1=Duel.IsExistingMatchingCard(s.tgfilter, tp, LOCATION_GRAVE, 0, 1, nil)
-	local b2=Duel.IsExistingMatchingCard(s.tgfilter2, tp, LOCATION_GRAVE, 0, 1, nil) and Duel.IsExistingMatchingCard(Card.IsCode, tp, LOCATION_ONFIELD, 0, 1, nil, 450000110)
+	local b2=Duel.IsExistingMatchingCard(s.tgfilter2, tp, LOCATION_GRAVE, 0, 1, nil) and Duel.IsExistingMatchingCard(s.staddreamsfilter, tp, LOCATION_ONFIELD, 0, 1, nil)
 
 	local op=aux.SelectEffect(tp, {b1,aux.Stringid(id,2)},
 									{b2,aux.Stringid(id,0)})
