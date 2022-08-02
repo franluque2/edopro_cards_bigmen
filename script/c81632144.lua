97a58a32901fa8143e93fb5fefeb05274dca1932
Fran Luque <franluque2@gmail.com>
2022-08-02 16:39:44 -0300

    gave up on aligator baby

--- script/c81632144.lua
+++ script/c81632144.lua
@@ -91,11 +91,11 @@ function s.nsop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 
 	if tc then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_EXTRA_RELEASE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
+		-- local e1=Effect.CreateEffect(c)
+		-- e1:SetType(EFFECT_TYPE_SINGLE)
+		-- e1:SetCode(EFFECT_EXTRA_RELEASE)
+		-- e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		-- c:RegisterEffect(e1)
 
 		if tc:IsSummonable(true,nil,1) then
 			Duel.BreakEffect()
