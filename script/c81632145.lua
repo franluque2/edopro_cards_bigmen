c1ac44b3c577bb270bf35db4e43421f9200b6d2f
Fran Luque <franluque2@gmail.com>
2022-08-02 12:31:56 -0300

    add giant kra corn and Release Change

---
+++ script/c81632145.lua
@@ -0,0 +1,96 @@
+--Giant Kra-Corn (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,511005044)
+	--Continual effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.tg)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+
+	--Your opponent cannot target monsters you control for attacks except this one.
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetValue(s.atlimit)
+	c:RegisterEffect(e3)
+
+	--Special Summon from Graveyard
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(s.spcon)
+	e2:SetCost(s.spcost)
+	e2:SetOperation(s.spop)
+	e2:SetLabel(0)
+	c:RegisterEffect(e2)
+
+
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.sptg2)
+	e4:SetOperation(s.spop2)
+	c:RegisterEffect(e4)
+end
+function s.fil(c)
+	return c:IsRace(RACE_PLANT) and c:IsPosition(POS_FACEUP_ATTACK)
+end
+function s.tg(e,c)
+	return s.fil(c)
+end
+function s.val(e,c)
+	return Duel.GetMatchingGroup(s.fil,c:GetControler(),LOCATION_MZONE,0,c):GetSum(Card.GetBaseAttack)
+end
+function s.atlimit(e,c)
+	return c~=e:GetHandler()
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_DESTROY)
+end
+function s.filter(c)
+	return c:IsRace(RACE_PLANT)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.filter,1,false,nil,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.filter,1,1,false,nil,nil)
+	Duel.Release(g,REASON_COST)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP_DEFENSE)
+end
+
+
+function s.filter2(c,e,tp)
+	return (c:IsCode(511009158) or c:IsCode(73776643)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter2(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
