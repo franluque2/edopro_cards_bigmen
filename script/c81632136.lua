bbf007fde0b7a07b88f1d513fcd47d0d592d9e8a
Fran Luque <franluque2@gmail.com>
2022-09-24 22:04:02 -0300

    Fix bug with Mutant Lycanthrope

--- script/c81632136.lua
+++ script/c81632136.lua
@@ -44,7 +44,7 @@ end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsControlerCanBeChanged() end
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsControlerCanBeChanged, tp, 0, LOCATION_MZONE, 1, nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 	local g=Duel.SelectTarget(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
