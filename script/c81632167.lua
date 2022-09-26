992ac2d6691caec3801eea47cdc56addc72af4e2
Fran Luque <franluque2@gmail.com>
2022-09-26 00:23:48 -0300

    Minor Bugfixes

--- script/c81632167.lua
+++ script/c81632167.lua
@@ -44,7 +44,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 	local e5=e4:Clone()
 	e5:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
-	e4:SetCondition(s.con2)
+	e5:SetCondition(s.con2)
 	e5:SetValue(aux.imval1)
 	c:RegisterEffect(e5)
 end
