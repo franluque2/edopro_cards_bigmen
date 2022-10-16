c48a02a1d370c11a33499ad7d25ffcc303b574f0
Fran Luque <franluque2@gmail.com>
2022-10-16 02:22:01 -0300

    add first few Granel Cards

--- script/c81632200.lua
+++ script/c81632200.lua
@@ -13,8 +13,43 @@ function s.initial_effect(c)
 	e1:SetCondition(s.sdcon)
 	c:RegisterEffect(e1)
 
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCountLimit(1)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.tg)
+	e3:SetCondition(s.indescon)
+	e3:SetValue(aux.indoval)
+	c:RegisterEffect(e3)
+
 end
 s.listed_series={0x3013}
 function s.sdcon(e)
 	return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x3013),0,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+end
+
+function s.val(e,re,r,rp)
+	return (r&REASON_BATTLE)~=0
+end
+
+function s.tg(e,c)
+	return c:IsFaceup() and (c:IsSetCard(0x3013))
+end
+
+function s.filter(c)
+	return not c:IsControler(c:GetOwner())
+end
+
+function s.indescon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_ONFIELD, 0, 1,nil)
 end
