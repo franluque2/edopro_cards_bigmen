0a020200ab9df5ad5921fae95849dbf335bcbdea
Fran Luque <franluque2@gmail.com>
2022-11-29 21:30:30 -0300

    Fixes to the conscriptions

--- script/c81632976.lua
+++ script/c81632976.lua
@@ -67,7 +67,7 @@ end
 
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1 and Duel.GetFlagEffect(tp, id)==0
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
