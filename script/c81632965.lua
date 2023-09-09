61e57669c835eae7d8593da97e21fd0e8c03c242
Fran Luque <franluque2@gmail.com>
2023-09-09 15:48:56 -0300

    Possible fix for akos skill

--- script/c81632965.lua
+++ script/c81632965.lua
@@ -232,7 +232,7 @@ function s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
             while tc do
                 local e1=Effect.CreateEffect(e:GetHandler())
                 e1:SetType(EFFECT_TYPE_SINGLE)
-                e1:SetCode(EFFECT_CHANGE_LEVEL)
+                e1:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
                 e1:SetValue(card:Level())
                 e1:SetReset(RESET_EVENT+RESETS_STANDARD)
                 tc:RegisterEffect(e1)
