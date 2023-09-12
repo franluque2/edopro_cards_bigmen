d8f5ccf54cf86d373f4c613a10cb5cf905c4e6ad
Skelecrit3129 <60719734+Skelecrit3129@users.noreply.github.com>
2023-09-12 19:30:32 +0100

    Yomi Fix

--- script/c81633032.lua
+++ script/c81633032.lua
@@ -26,11 +26,13 @@ local LOCATIONS=LOCATION_ALL-LOCATION_OVERLAY
 local ARCHETYPE=0x1186
 
 --add the conditions for the archetype swap here
-function s.archetypefilter(c)
-  return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_DINOSAUR)
+function s.Birds(c)
+  return c:IsCode(511002897, 511005630, 511009137)
 end
 
-
+function s.Fiends(c)
+  return c:IsOriginalRace(RACE_FIEND) and c:IsOriginalAttribute(ATTRIBUTE_DARK)
+end
 
 
 
@@ -50,8 +52,32 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
         e5:SetCode(EFFECT_ADD_SETCODE)
         e5:SetTargetRange(LOCATIONS,0)
         e5:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
-        e5:SetValue(ARCHETYPE)
+        e5:SetValue(0x563)
         Duel.RegisterEffect(e5,tp)
+		
+		local e6=Effect.CreateEffect(e:GetHandler())
+        e6:SetType(EFFECT_TYPE_FIELD)
+        e6:SetCode(EFFECT_ADD_RACE)
+        e6:SetTargetRange(LOCATIONS,0)
+        e6:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
+        e6:SetValue(RACE_FIEND)
+        Duel.RegisterEffect(e6,tp)
+
+		local e7=Effect.CreateEffect(e:GetHandler())
+        e7:SetType(EFFECT_TYPE_FIELD)
+        e7:SetCode(EFFECT_ADD_ATTRIBUTE)
+        e7:SetTargetRange(LOCATIONS,0)
+        e7:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
+        e7:SetValue(ATTRIBUTE_DARK)
+        Duel.RegisterEffect(e7,tp)
+
+		local e8=Effect.CreateEffect(e:GetHandler())
+        e8:SetType(EFFECT_TYPE_FIELD)
+        e8:SetCode(EFFECT_ADD_SETCODE)
+        e8:SetTargetRange(LOCATIONS,0)
+        e8:SetTarget(function(_,c)  return c:IsHasEffect(id+1) end)
+        e8:SetValue(0x563)
+        Duel.RegisterEffect(e8,tp)
     
 
 
@@ -161,7 +187,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 
-    local g=Duel.GetMatchingGroup(s.archetypefilter, tp, LOCATION_ALL, LOCATION_ALL, nil)
+    local g=Duel.GetMatchingGroup(s.Birds, tp, LOCATION_ALL, LOCATION_ALL, nil)
 
     if #g>0 then
 		local tc=g:GetFirst()
@@ -170,6 +196,22 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 				local e3=Effect.CreateEffect(e:GetHandler())
 				e3:SetType(EFFECT_TYPE_SINGLE)
 				e3:SetCode(id)
+				tc:RegisterEffect(e3)
+
+
+			tc=g:GetNext()
+		end
+	end
+
+	g=Duel.GetMatchingGroup(s.Fiends, tp, LOCATION_ALL, LOCATION_ALL, nil)
+
+    if #g>0 then
+		local tc=g:GetFirst()
+		while tc do
+			
+				local e3=Effect.CreateEffect(e:GetHandler())
+				e3:SetType(EFFECT_TYPE_SINGLE)
+				e3:SetCode(id+1)
 				tc:RegisterEffect(e3)
 
 
