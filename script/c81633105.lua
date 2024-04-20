f05a50e8ba67db97ef3692d1fe1938bb5faf299d
Fran Luque <franluque2@gmail.com>
2024-04-19 23:01:04 -0300

    Fix

--- script/c81633105.lua
+++ script/c81633105.lua
@@ -23,7 +23,6 @@ end
 local LOCATIONS=LOCATION_ALL-LOCATION_OVERLAY
 
 --add archetype setcode here
-local ARCHETYPE=0x1186
 
 --add the conditions for the archetype swap here
 function s.archetypefilter(c)
@@ -31,7 +30,7 @@ function s.archetypefilter(c)
 end
 
 function s.archetypefilter2(c)
-  return c:IsSetCard(0xe5)
+  return c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 
 
@@ -51,17 +50,17 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 
         local e5=Effect.CreateEffect(e:GetHandler())
         e5:SetType(EFFECT_TYPE_FIELD)
-        e5:SetCode(EFFECT_CHANGE_SETCODE)
+        e5:SetCode(EFFECT_ADD_SETCODE)
         e5:SetTargetRange(LOCATIONS,0)
-        e5:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
-        e5:SetValue(ARCHETYPE)
+        e5:SetTarget(function(_,c)  return s.archetypefilter(c) end)
+        e5:SetValue(0xe5)
         Duel.RegisterEffect(e5,tp)
 
         local e6=Effect.CreateEffect(e:GetHandler())
         e6:SetType(EFFECT_TYPE_FIELD)
         e6:SetCode(EFFECT_ADD_SETCODE)
         e6:SetTargetRange(LOCATION_DECK+LOCATION_GRAVE,0)
-        e6:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
+        e6:SetTarget(function(_,c)  return s.archetypefilter2(c) end)
         e6:SetValue(0x7b)
         Duel.RegisterEffect(e6,tp)
     
@@ -82,38 +81,6 @@ end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-
-    local g=Duel.GetMatchingGroup(s.archetypefilter, tp, LOCATION_ALL, LOCATION_ALL, nil)
-
-    if #g>0 then
-		local tc=g:GetFirst()
-		while tc do
-			
-				local e3=Effect.CreateEffect(e:GetHandler())
-				e3:SetType(EFFECT_TYPE_SINGLE)
-				e3:SetCode(id)
-				tc:RegisterEffect(e3)
-
-
-			tc=g:GetNext()
-		end
-	end
-
-    g=Duel.GetMatchingGroup(s.archetypefilter2, tp, LOCATION_ALL, LOCATION_ALL, nil)
-
-    if #g>0 then
-		local tc=g:GetFirst()
-		while tc do
-			
-				local e3=Effect.CreateEffect(e:GetHandler())
-				e3:SetType(EFFECT_TYPE_SINGLE)
-				e3:SetCode(id+1)
-				tc:RegisterEffect(e3)
-
-
-			tc=g:GetNext()
-		end
-	end
 
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
 end
