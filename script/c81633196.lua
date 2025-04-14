ced68db7962d0f45b6d96662a34326171e0609df
Skelecrit3129 <60719734+Skelecrit3129@users.noreply.github.com>
2025-04-14 15:46:06 +0100

    Skill Fix

--- script/c81633196.lua
+++ script/c81633196.lua
@@ -71,7 +71,7 @@ function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
 
     local g=Duel.GetMatchingGroup(s.sumfilter, tp, LOCATION_HAND, 0, nil, e,tp)
     if #g>0 and Duel.SelectYesNo(tp, aux.Stringid(id, 2)) then
-        local g2=g1:Select(tp, 1,3,nil)
+        local g2=g:Select(tp, 1,3,nil)
         if g2 then
             Duel.SpecialSummon(g2, SUMMON_TYPE_SPECIAL, tp, tp, false,false, POS_FACEUP)
             for tc in g2:Iter() do
