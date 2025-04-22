f6ff23db99633fa06d35ef6136bc9d026958d0e1
Fran Luque <franluque2@gmail.com>
2025-04-22 04:58:44 -0300

    Add new rush proc

--- script/big_aux.lua
+++ script/big_aux.lua
@@ -202,6 +202,231 @@ function bRush.addrules()
   end
 end
 
+bRush.CreateConjureRitualProc = aux.FunctionWithNamedArgs(
+function(c,_type,conjurename,filter,lv,desc,extrafil,extraop,matfilter,stage2,location,forcedselection,customoperation,specificmatfilter,requirementfunc,sumpos,extratg)
+	if filter and type(filter)=="function" then
+		local mt=c.__index
+		if not mt.ritual_matching_function then
+			mt.ritual_matching_function={}
+		end
+		mt.ritual_matching_function[c]=filter
+	end
+
+  if not conjurename then
+    return
+  end
+
+
+  local ge1=Effect.CreateEffect(c)
+  ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+  ge1:SetCode(EVENT_PREDRAW)
+  ge1:SetCountLimit(1)
+  ge1:SetLabel(conjurename)
+  ge1:SetOperation(bRush.checkop)
+  Duel.RegisterEffect(ge1,0)
+
+
+	local e1=Effect.CreateEffect(c)
+	if desc then
+		e1:SetDescription(desc)
+	else
+		e1:SetDescription(1171)
+	end
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetLabel(conjurename)
+	e1:SetTarget(bRush.ritConjureTarget(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,specificmatfilter,requirementfunc,sumpos,extratg))
+	e1:SetOperation(bRush.ritConjureOp(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,customoperation,specificmatfilter,requirementfunc,sumpos))
+	return e1
+end,"handler","lvtype","conjurename","filter","lv","desc","extrafil","extraop","matfilter","stage2","location","forcedselection","customoperation","specificmatfilter","requirementfunc","sumpos","extratg")
+
+bRush.RitualsToSummon={}
+bRush.RitualsToSummon[0]={}
+bRush.RitualsToSummon[1]={}
+
+bRush.checkop= function(e)
+  for tp = 0, 1, 1 do
+    if Duel.GetFlagEffect(tp, e:GetHandler():GetOriginalCode())==0 then
+      Duel.RegisterFlagEffect(tp, e:GetHandler():GetOriginalCode(), 0, 0, 0)
+      local token=Duel.CreateToken(tp, e:GetLabel())
+      Duel.SendtoDeck(token, nil, -2, REASON_RULE)
+      bRush.RitualsToSummon[tp][e:GetLabel()]=token
+  end
+  end
+end
+
+
+bRush.getRitualConjureTarget=function (code,tp)
+  return bRush.RitualsToSummon[tp][code]
+end
+
+local function ExtraReleaseFilter(c,tp)
+	return c:IsControler(1-tp) and c:IsHasEffect(EFFECT_EXTRA_RELEASE)
+end
+
+local function ForceExtraRelease(mg)
+	return function(e,tp,g,c)
+		return g:Includes(mg)
+	end
+end
+
+bRush.ritConjureTarget = aux.FunctionWithNamedArgs(
+function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,specificmatfilter,requirementfunc,sumpos,extratg)
+	location = location or LOCATION_EXTRA
+	sumpos = sumpos or POS_FACEUP
+	return	function(e,tp,eg,ep,ev,re,r,rp,chk)
+				if chk==0 then
+					local mg=Duel.GetRitualMaterial(tp,not requirementfunc)
+					local mg2=extrafil and extrafil(e,tp,eg,ep,ev,re,r,rp,chk) or Group.CreateGroup()
+					--if an EFFECT_EXTRA_RITUAL_MATERIAL effect has a forcedselection of its own
+					--add that forcedselection to the one of the Ritual Spell, if any
+					local extra_eff_g=mg:Filter(Card.IsHasEffect,nil,EFFECT_EXTRA_RITUAL_MATERIAL)
+					local func=forcedselection
+					--if a card controlled by the opponent has EFFECT_EXTRA_RELEASE, then it MUST be
+					--used as material
+					local extra_mat_g=mg:Filter(ExtraReleaseFilter,nil,tp)
+					if #extra_mat_g>0 then
+						func=MergeForcedSelection(ForceExtraRelease(extra_mat_g),func)
+					end
+					if #extra_eff_g>0 then
+						local prev_repl_function=nil
+						for tmp_c in extra_eff_g:Iter() do
+							local effs={tmp_c:IsHasEffect(EFFECT_EXTRA_RITUAL_MATERIAL)}
+							for _,eff in ipairs(effs) do
+								local repl_function=eff:GetLabelObject()
+								if repl_function and prev_repl_function~=repl_function[1] then
+									prev_repl_function=repl_function[1]
+									func=MergeForcedSelection(func,repl_function[1])
+								end
+							end
+						end
+					end
+					Ritual.CheckMatFilter(matfilter,e,tp,mg,mg2)
+          return Ritual.Filter(bRush.getRitualConjureTarget(e:GetLabel(),tp),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
+				end
+				if extratg then extratg(e,tp,eg,ep,ev,re,r,rp,chk) end
+				Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,location)
+			end
+end,"filter","lvtype","lv","extrafil","extraop","matfilter","stage2","location","forcedselection","specificmatfilter","requirementfunc","sumpos","extratg")
+
+local function MergeForcedSelection(f1,f2)
+	if f1==nil or f2==nil then
+		return f1 or f2
+	end
+	return function(...)
+		local ret1,ret2=f1(...)
+		local repl1,repl2=f2(...)
+		return ret1 and repl1,ret2 or repl2
+	end
+end
+
+bRush.ritConjureOp = aux.FunctionWithNamedArgs(
+function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,customoperation,specificmatfilter,requirementfunc,sumpos)
+	location = location or LOCATION_EXTRA
+	sumpos = sumpos or POS_FACEUP
+	return	function(e,tp,eg,ep,ev,re,r,rp)
+				local mg=Duel.GetRitualMaterial(tp,not requirementfunc)
+				local mg2=extrafil and extrafil(e,tp,eg,ep,ev,re,r,rp) or Group.CreateGroup()
+				--if an EFFECT_EXTRA_RITUAL_MATERIAL effect has a forcedselection of its own
+				--add that forcedselection to the one of the Ritual Spell, if any
+				local func=forcedselection
+				local extra_eff_g=mg:Filter(Card.IsHasEffect,nil,EFFECT_EXTRA_RITUAL_MATERIAL)
+				if #extra_eff_g>0 then
+					local prev_repl_function=nil
+					for tmp_c in extra_eff_g:Iter() do
+						local effs={tmp_c:IsHasEffect(EFFECT_EXTRA_RITUAL_MATERIAL)}
+						for _,eff in ipairs(effs) do
+							local repl_function=eff:GetLabelObject()
+							if repl_function and prev_repl_function~=repl_function[1] then
+								prev_repl_function=repl_function[1]
+								func=MergeForcedSelection(func,repl_function[1])
+							end
+						end
+					end
+				end
+				--if a card controlled by the opponent has EFFECT_EXTRA_RELEASE, then it MUST be
+				--used as material
+				local extra_mat_g=mg:Filter(ExtraReleaseFilter,nil,tp)
+				if #extra_mat_g>0 then
+					func=MergeForcedSelection(ForceExtraRelease(extra_mat_g),func)
+				end
+				Ritual.CheckMatFilter(matfilter,e,tp,mg,mg2)
+				local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local tg=Group.FromCards(bRush.getRitualConjureTarget(e:GetLabel(),tp))
+				if #tg>0 then
+					local tc=tg:GetFirst()
+					local lv=(lv and (type(lv)=="function" and lv(tc)) or lv) or tc:GetLevel()
+					lv=math.max(1,lv)
+					Ritual.SummoningLevel=lv
+					local mat=nil
+					mg:Match(Card.IsCanBeRitualMaterial,tc,tc)
+					mg:Merge(mg2-tc)
+					if specificmatfilter then
+						mg:Match(specificmatfilter,nil,tc,mg,tp)
+					end
+					if tc.ritual_custom_operation then
+						tc:ritual_custom_operation(mg,func,_type)
+						mat=tc:GetMaterial()
+					else
+						func=MergeForcedSelection(tc.ritual_custom_check,func)
+						if tc.mat_filter then
+							mg:Match(tc.mat_filter,tc,tp)
+						end
+						if ft>0 and not func then
+							Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+							if _type==RITPROC_EQUAL then
+								mat=mg:SelectWithSumEqual(tp,requirementfunc or Card.GetRitualLevel,lv,1,#mg,tc)
+							else
+								mat=mg:SelectWithSumGreater(tp,requirementfunc or Card.GetRitualLevel,lv,tc)
+							end
+						else
+							mat=aux.SelectUnselectGroup(mg,e,tp,1,lv,Ritual.Check(tc,lv,WrapTableReturn(func),_type,requirementfunc),1,tp,HINTMSG_RELEASE,Ritual.Finishcon(tc,lv,WrapTableReturn(func),requirementfunc,_type))
+						end
+					end
+					--check if a card from an "once per turn" EFFECT_EXTRA_RITUAL_MATERIAL effect was selected
+					local extra_eff_g=mat:Filter(Card.IsHasEffect,nil,EFFECT_EXTRA_RITUAL_MATERIAL)
+					for tmp_c in extra_eff_g:Iter() do
+						local effs={tmp_c:IsHasEffect(EFFECT_EXTRA_RITUAL_MATERIAL)}
+						for _,eff in ipairs(effs) do
+							--if eff is OPT and tmp_c is not returned
+							--by the Ritual Spell's exrafil
+							--then use the count limit and register
+							--the flag to turn the extra eff OFF
+							--requires the EFFECT_EXTRA_RITUAL_MATERIAL effect
+							--to check the flag in its condition
+							local _,max_count_limit=eff:GetCountLimit()
+							if max_count_limit>0 and not mg2:IsContains(tmp_c) then
+								eff:UseCountLimit(tp,1)
+								Duel.RegisterFlagEffect(tp,eff:GetHandler():GetCode(),RESET_PHASE+PHASE_END,0,1)
+							end
+						end
+					end
+					if not customoperation then
+            local tc2=Duel.CreateToken(tp, tc:GetOriginalCode())
+						tc2:SetMaterial(mat)
+						if extraop then
+							extraop(mat:Clone(),e,tp,eg,ep,ev,re,r,rp,tc2)
+						else
+							Duel.ReleaseRitualMaterial(mat)
+						end
+						Duel.BreakEffect()
+						Duel.SpecialSummon(tc2,SUMMON_TYPE_RITUAL,tp,tp,false,true,sumpos)
+						tc2:CompleteProcedure()
+						if tc:IsFacedown() then Duel.ConfirmCards(1-tp,tc2) end
+						if stage2 then
+							stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc2)
+						end
+					else
+						customoperation(mat:Clone(),e,tp,eg,ep,ev,re,r,rp,tc2)
+					end
+					Ritual.SummoningLevel=nil
+				end
+			end
+end,"filter","lvtype","lv","extrafil","extraop","matfilter","stage2","location","forcedselection","customoperation","specificmatfilter","requirementfunc","sumpos")
+
+
 function bRush.getcarddraw(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0) < 5 then
 		return 5-Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0)
