a096c6f8207290eb0f7d9aaa2a8ce61935e51ded
Fran Luque <franluque2@gmail.com>
2025-04-27 15:25:11 -0300

    Naturia fix

--- script/c81633200.lua
+++ script/c81633200.lua
@@ -55,10 +55,11 @@ end
 
 function s.effectfilter(e,ct)
 	local p=e:GetHandlerPlayer()
-	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
-	return p==(1-tp)
+	local de,dp=Duel.GetChainInfo(ev,CHAININFO_DISABLE_REASON,CHAININFO_DISABLE_PLAYER)
+	return de and (dp==(1-p))
 end
 function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
 	return rp==tp
 end
 
